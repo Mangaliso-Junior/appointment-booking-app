5aeb059b4221ac952121b1a0c69b8ed547f093ba
Mangaliso-Junior <jymokoena@gmail.com>
2024-08-09 15:55:42 +0200

    changes made to the validation of the website

--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,25 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+from wtforms import Form, StringField, PasswordField, validators
+from wtforms.validators import ValidationError
+from .validators import email_validator #import the customer email validation
+
 auth = Blueprint('auth', __name__)
 
+class RegistrationForm(Form):
+    email = StringField('Email', [validators.DataRequired(), email_validator])
+    first_name = StringField('First Name', [validators.DataRequired(), validators.Length(min=2, max=150)])
+    last_name = StringField('Last Name', [validators.DataRequired(), validators.Length(min=2, max=150)])
+    password_1 = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.Length(min=7, max=150)
+    ])
+    password_2 = PasswordField('Confirm Password', [
+        validators.DataRequired(),
+        validators.EqualTo('password_1', message='Passwords must match')
+    ])
+    
 @auth.route('/login', methods=['GET', 'POST'])
 def login(): 
     if request.method == 'POST':
@@ -12,6 +29,11 @@ def login():
         password = request.form.get('password')
         
         user = User.query.filter_by(email=email).first()
+        try: 
+            email_validator(email)
+        except ValidationError as e: 
+            flash(str(e), category='error')
+            return render_template("sign-up.html", user=current_user)
         if user:
             if check_password_hash(user.password, password): 
                 flash('Logged in successfully!', category='success')
@@ -32,26 +54,16 @@ def logout():
 
 @auth.route('/sign-up',methods=['GET', 'POST'])
 def sign_up(): 
-    if request.method == 'POST': 
-        email = request.form.get('email')
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        password_1 = request.form.get('password_1')
-        password_2 = request.form.get('password_2')
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate(): 
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        password_1 = form.password_1.data
         
         user = User.query.filter_by(email=email).first()
         if user: 
             flash('Email already exists', category='error')
-        elif len(email) < 4: 
-            flash('Email must be greater than 3 characters', category='error')
-        elif len(first_name) < 2: 
-            flash('First name must be greater than 1 character', category='error')
-        elif len(last_name) < 2: 
-            flash('Last name must be greater than 1 character', category='error')
-        elif password_1 != password_2: 
-            flash('Passwords don\'t match', category='error')
-        elif len(password_1) < 7: 
-            flash('Passowrd must be at least 7 characters', category='error')
         else: 
             new_user = User(email=email, first_name = first_name, last_name = last_name, password=generate_password_hash(password_1, method='pbkdf2:sha256'))
             db.session.add(new_user)
---
+++ website/validators.py
@@ -0,0 +1,9 @@
+import re 
+from wtforms import ValidationError
+
+email_regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+def email_validator(form, field):
+    email = field.data 
+    if not email_regex.fullmatch(email): 
+        raise ValidationError("Invalid email address")
